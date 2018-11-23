536cf9c63e9803c3207b4ce9a66b83cdd890e3ef
coolpicube <45299375+coolpicube@users.noreply.github.com>
2018-11-23 17:49:09 -0500

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -49,7 +49,7 @@ def count_palindromes(input_file):
         line = line.rstrip()
         # Checks if each line is a palindrome
         if is_palindrome(line):
-            total_palindromes = 0
+            total_palindromes += 1
     return total_palindromes
 
 
